466bed59bf383f6164a34f93eedc32b6395e9c8d
Coen Claus <cclaus@4ps.nl>
2023-02-28 16:10:24 +0100

    broke spaghetti code

--- operators.py
+++ operators.py
@@ -429,7 +429,7 @@ class ConfirmSelection(bpy.types.Operator):
 
             for property_name, property_value in selection_configuration.items():
 
-                if property_value == True:
+                
                     bool_property = property_value 
                     bool_property_value = bool(bool_property)
 
@@ -442,17 +442,7 @@ class ConfirmSelection(bpy.types.Operator):
                     ifc_properties.my_ifcmaterial           = bool_property_value
 
 
-                if property_value == False:
-                    bool_property = property_value 
-                    bool_property_value = bool(bool_property)
-
-                    ifc_properties.my_ifcproduct            = bool_property_value
-                    ifc_properties.my_ifcproductname        = bool_property_value
-                    ifc_properties.my_ifcproducttypename    = bool_property_value
-                    ifc_properties.my_ifcbuildingstorey     = bool_property_value
-                    ifc_properties.my_ifcclassification     = bool_property_value
-                    ifc_properties.my_ifcbuildingstorey     = bool_property_value
-                    ifc_properties.my_ifcmaterial           = bool_property_value
+            
 
                     break
 
